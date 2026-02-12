c2213c5787b07f56236762bb00e74e50be499ada
Koushik Debnath <koushikcomhere@gmail.com>
2026-02-12 19:56:53 +0530

    changes

---
+++ backend/app/db/base.py
@@ -0,0 +1,8 @@
+from app.db.base_class import Base  # noqa
+from app.models.user import User  # noqa
+from app.models.resume import Resume  # noqa
+from app.models.skill import Skill  # noqa
+from app.models.company import Company, CompanySkillRequirement  # noqa
+from app.models.user_skill_score import UserSkillScore  # noqa
+from app.models.roadmap import Roadmap, RoadmapTask  # noqa
+from app.models.progress_log import ProgressLog  # noqa

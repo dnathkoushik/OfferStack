680974fc288f848b46e61c9716588cd6ff039964
Koushik Debnath <koushikcomhere@gmail.com>
2026-02-15 11:12:18 +0530

    changes

---
+++ backend/app/api/deps.py
@@ -0,0 +1,7 @@
+
+from typing import AsyncGenerator
+from app.db.session import SessionLocal
+
+async def get_db() -> AsyncGenerator:
+    async with SessionLocal() as session:
+        yield session

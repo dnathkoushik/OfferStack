0c501b02374ac9c3da07a813e1cb64f15d9d1b05
Koushik Debnath <koushikcomhere@gmail.com>
2026-02-16 17:21:25 +0530

    .

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import resume
+from app.api.v1.endpoints import resume, login
 
 api_router = APIRouter()
+api_router.include_router(login.router, tags=["login"])
 api_router.include_router(resume.router, prefix="/resumes", tags=["resumes"])
